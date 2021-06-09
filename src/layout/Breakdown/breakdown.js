c8efbab6290bfb0ac672304c0e900ae6bedc968d
chandupatlasandeep <chandupatlasandeep1990@gmail.com>
2021-06-09 12:40:32 +0530

    Modified code in breakdown

--- src/layout/Breakdown/breakdown.js
+++ src/layout/Breakdown/breakdown.js
@@ -52,9 +52,14 @@ export default function Breakdown() {
     const [executive,setExecutive] = useState("")
     const [reports,setReports] = useState("")
     const [alert,setAlert] = useState(false)
+    const [editAlert,setEditAlert] = useState(false)
+
+    const [deleteAlert,setDeleteAlert] = useState(false)
+    const [updateId, setUpdateId] = useState()
+
     const [info, setInfo] = useState(false)
     const [editModal, setEditModal] = useState(false)
-    const [edit, setEdit] = useState(false)
+    
 
     const submitHandler = () => {
         let currentData = {}
@@ -75,15 +80,26 @@ export default function Breakdown() {
         setAlert(true)
     }
     const editBtnHandler = () => {   
-      const update = {...edit}
-      const copiedList = [...data]   
-      const element = copiedList.find(elem => elem.id === update.id)
-      element.breakdownId = update.breakdownId;
-      element.description = update.description;
-        setData(copiedList)
+      let updatedData = {}
+        updatedData.id = updateId
+        updatedData.breakdownId = breakdownId
+        updatedData.status = status
+        updatedData.issueType=issueType
+        updatedData.description=description
+        updatedData.company=company
+        updatedData.contactNumber=contactNumber
+        updatedData.executive=executive
+        updatedData.reports=reports
+        console.log('updatedData', updatedData)
+        let filteredArr = data.filter(function( obj ) {
+          return obj.id !== updateId;
+        });
+        console.log(filteredArr)
+        setData([...filteredArr, updatedData])
+
         setEditModal(false)
-        setAlert(true)
-      
+        setEditAlert(true)
+       
    }
     const conditionalRowStyles = [
       {
@@ -97,6 +113,17 @@ export default function Breakdown() {
         },
       }
     ];
+    const deleteHandler = () => {
+      let element = [...data]
+      let updatedData = {}
+      updatedData.id = updateId
+      console.log(updatedData.id);
+      element = element.filter(item => item.id !==updatedData.id);
+      setData(element)
+      setEditModal(false)
+
+      setDeleteAlert(true)
+  }
     
   return (
     <>
@@ -114,7 +141,7 @@ export default function Breakdown() {
               Breakdown
               </CCol>
               <CCol xs="1">
-              <CButton block variant="ghost" color="info" onClick={() => setInfo(!info)} className="mr-1">New</CButton>
+              <CButton  color="info" onClick={() => setInfo(!info)} className="mr-1">New</CButton>
             </CCol>
             </CRow>
             
@@ -128,7 +155,20 @@ export default function Breakdown() {
                 'breakdownId':
                   (item)=>(
                     <td>
-                   <a  onClick={()=> setEditModal(!editModal)}>{item.breakdownId}</a>
+                   <a  onClick={()=>{
+                     setUpdateId(item.id)
+                     setBreakdownId(item.breakdownId)
+                     setCompany(item.company)
+                     setIssueType(item.issueType)
+                     setDescription(item.description)
+                     setStatus(item.status)
+                     setExecutive(item.executive)
+                     setContactNumber(item.contactNumber)
+                     setReports(item.reports)
+
+                      setEditModal(!editModal)}
+                   }
+                      >{item.breakdownId}</a>
                     </td>
                   )
               }}
@@ -153,12 +193,7 @@ export default function Breakdown() {
               </CModalHeader>
               <CModalBody>
               <CRow>
-            <CCol xs="10" md="6">
-              <CFormGroup >
-                <CLabel htmlFor="breakdownId">Breakdown Id</CLabel>
-                <CInput type="text" id="breakdownId" name="breakdownId" placeholder="BreakdownId" value={breakdownId} onChange={(e) => setBreakdownId(e.target.value)}/>
-              </CFormGroup>
-            </CCol>
+       
             <CCol xs="10" md="6">
               <CFormGroup >
                 <CLabel htmlFor="company">Company</CLabel>
@@ -218,31 +253,32 @@ export default function Breakdown() {
             <CModal show={alert} variant="success" onClose={() => setAlert(false)} dismissible>
             <CModalHeader closeButton onClick={() => setAlert(false)}>Successfully Added!</CModalHeader>
           </CModal>
+
+          <CModal show={editAlert} variant="success" onClose={() => setEditAlert(false)} dismissible>
+            <CModalHeader closeButton onClick={() => setEditAlert(false)}>Updated Successfully</CModalHeader>
+          </CModal>
+
+          <CModal show={deleteAlert} variant="success" onClose={() => setDeleteAlert(false)} dismissible>
+            <CModalHeader closeButton onClick={() => setDeleteAlert(false)}>Deleted Successfully</CModalHeader>
+          </CModal>
             </CCardBody>
           </CCard>
         </CCol>
-        </CRow>
-
         <CModal 
               show={editModal} 
               onClose={() => setEditModal(!editModal)}
               color="info"
             >
               <CModalHeader closeButton>
-                <CModalTitle>EditModal Breakdown</CModalTitle>
+                <CModalTitle>Edit Breakdown</CModalTitle>
               </CModalHeader>
               <CModalBody>
               <CRow>
-            <CCol xs="10" md="6">
-              <CFormGroup >
-                <CLabel htmlFor="breakdownId">Breakdown Id</CLabel>
-                <CInput type="text" id="breakdownId1" name="breakdownId" placeholder="BreakdownId" value={edit.breakdownId} onChange={(e) => setEdit({...edit, breakdownId: e.target.value})}/>
-              </CFormGroup>
-            </CCol>
+      
             <CCol xs="10" md="6">
               <CFormGroup >
                 <CLabel htmlFor="company">Company</CLabel>
-                <CInput type="text" id="company" name="company" placeholder="company" value={company} onChange={(e) => setCompany(e.target.value)}/>
+                <CInput type="text" id="company" name="company" placeholder="company" value={company? company: ''} onChange={(e) => setCompany(e.target.value)}/>
               </CFormGroup>
             </CCol>
             </CRow>
@@ -250,13 +286,13 @@ export default function Breakdown() {
             <CCol xs="10" md="6">
               <CFormGroup >
                 <CLabel htmlFor="description">Description</CLabel>
-                <CInput type="text" id="description" name="description" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}/>
+                <CInput type="text" id="description" name="description" placeholder="Description" value={description? description: ''} onChange={(e) => setDescription(e.target.value)}/>
               </CFormGroup>
             </CCol>
             <CCol xs="10" md="6">
               <CFormGroup >
                 <CLabel htmlFor="issueType">Issue Type</CLabel>
-                <CInput type="text" id="issueType" name="issueType" placeholder="Issue Type" value={issueType} onChange={(e) => setIssueType(e.target.value)}/>
+                <CInput type="text" id="issueType" name="issueType" placeholder="Issue Type" value={issueType?issueType: ''} onChange={(e) => setIssueType(e.target.value)}/>
               </CFormGroup>
             </CCol>
             </CRow>
@@ -264,13 +300,13 @@ export default function Breakdown() {
             <CCol xs="10" md="6">
               <CFormGroup >
                 <CLabel htmlFor="executive">Executive</CLabel>
-                <CInput type="text" id="executive" name="executive" placeholder="Executive" value={executive} onChange={(e) => setExecutive(e.target.value)}/>
+                <CInput type="text" id="executive" name="executive" placeholder="Executive" value={executive? executive: ''} onChange={(e) => setExecutive(e.target.value)}/>
               </CFormGroup>
             </CCol>
             <CCol xs="10" md="6">
               <CFormGroup >
                 <CLabel htmlFor="status">Status</CLabel>
-                <CInput type="text" id="status" name="status" placeholder="Status" value={status} onChange={(e) => setStatus(e.target.value)}/>
+                <CInput type="text" id="status" name="status" placeholder="Status" value={status? status: ''} onChange={(e) => setStatus(e.target.value)}/>
               </CFormGroup>
             </CCol>
             </CRow>
@@ -278,22 +314,25 @@ export default function Breakdown() {
             <CCol xs="10" md="6">
               <CFormGroup >
                 <CLabel htmlFor="contactNumber">Contact Number</CLabel>
-                <CInput type="text" id="contactNumber" name="contactNumber" placeholder="Contact Number" value={contactNumber} onChange={(e) => setContactNumber(e.target.value)}/>
+                <CInput type="text" id="contactNumber" name="contactNumber" placeholder="Contact Number" value={contactNumber?contactNumber: ''} onChange={(e) => setContactNumber(e.target.value)}/>
               </CFormGroup>
             </CCol>
             <CCol xs="10" md="6">
               <CFormGroup >
                 <CLabel htmlFor="reports">Reports</CLabel>
-                <CInput type="text" id="reports" name="reports" placeholder="Reports" value={reports} onChange={(e) => setReports(e.target.value)}/>
+                <CInput type="text" id="reports" name="reports" placeholder="Reports" value={reports?reports: ''} onChange={(e) => setReports(e.target.value)}/>
               </CFormGroup>
             </CCol>
             </CRow>
               </CModalBody>
               <CModalFooter>
-                <CButton color="secondary" onClick={() => setInfo(!info)}>Delete</CButton>
+                <CButton color="secondary" onClick={ deleteHandler}>Delete</CButton>
                 <CButton color="info" onClick={editBtnHandler}>Edit</CButton>{' '}
               </CModalFooter>
             </CModal>
+        </CRow>
+
+
     </>
   )
 }       
