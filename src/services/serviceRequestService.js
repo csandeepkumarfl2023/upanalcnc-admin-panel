227917823a587a43df99ef0ad42c12639f7a57d1
csandeepkumardev01 <chandupatlasandeep1990@gmail.com>
2021-06-15 14:12:01 +0530

    modified

--- src/services/serviceRequestService.js
+++ src/services/serviceRequestService.js
@@ -3,7 +3,7 @@ import { constants } from '../constants';
 let serviceReqData = [
     {id: 0, servicerequestId: 'UPNLBKN202101', status:'Pending',priority:'High', issueType:'Electrical',description:'value proposition', contactNumber: '8765964234', 
     executive:'Assign',reports:'no', company: 'Aravindan', email: 'aravindan@gmail.com',createdDate: 'June 14th 2021, 1:23:09 pm', machine: 0, customerName: 0},
-    {id: 2, servicerequestId: 'UPNLBKN202115', status:'new',priority:'High', issueType:'Electrical',description:'value proposition', contactNumber: '8765964234', 
+    {id: 2, servicerequestId: 'UPNLBKN202115', status:'new',priority:'High', issueType:'Mechanical',description:'value proposition', contactNumber: '8765964234', 
     executive:'Assign',reports:'no', company: 'Aravindan', email: 'aravindan@gmail.com',createdDate: 'June 14th 2021, 1:23:09 pm', machine: 0, customerName: 0},
     {id: 3, servicerequestId: 'UPNLBKN202116', status:'Overdue',priority:'High', issueType:'Electrical',description:'value proposition', contactNumber: '8765964234', 
     execuive:'Assign',reports:'no', company: 'Aravindan', email: 'aravindan@gmail.com',createdDate: 'June 14th 2021, 1:23:09 pm', machine: 0, customerName: 0}
