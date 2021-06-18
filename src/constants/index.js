b42346b2b5ddc3a283189db9e9d7cf0df689090d
csandeepkumardev01 <64784427+csandeepkumardev01@users.noreply.github.com>
2021-06-18 21:57:49 +0530

    fixed issues

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const constants = {
-    apiEndPoint : 'http://ec2-13-127-148-151.ap-south-1.compute.amazonaws.com:8080/v1/api/'
+    apiEndPoint : 'http://backend.startso.in:8080/v1/api/'
 }
