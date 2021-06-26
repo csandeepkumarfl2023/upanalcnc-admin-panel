06d62eaff3cd1724790ba2f4d1cee500de4d31fc
csandeepkumardev01 <64784427+csandeepkumardev01@users.noreply.github.com>
2021-06-26 14:26:02 +0530

    added new token

--- src/helper/axios.helper.js
+++ src/helper/axios.helper.js
@@ -6,7 +6,7 @@ import Axios from "axios";
 Axios.interceptors.request.use(async (config) => {
     // let token = await commonService.getToken('userToken')
     // if(token){
-        config.headers['X-Authorization'] = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiUk9MRVMiOlsiQURNSU4iXSwiaXNzIjoiaHR0cDovL3VwYW5hbC5jb20iLCJpYXQiOjE2MjQwOTA0NjcsImV4cCI6MTYyNDY5MDQ2N30.fXjQ4foiI2Q6XWKo0JCgOkwyIt0VLXDswutojSM2sh2agAdDTOrUjr2oG2hMfDR-GasFCo36UqPW80bUSMoSoA`;
+        config.headers['X-Authorization'] = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiUk9MRVMiOlsiQURNSU4iXSwiaXNzIjoiaHR0cDovL3VwYW5hbC5jb20iLCJpYXQiOjE2MjQ2OTc1MTAsImV4cCI6MTYyNTI5NzUxMH0.Txbn-caKsMU8zPsVyrcteaFBobvMVaBYCTQGeWuT3lafCFJCqMk2fGyeR-Ze_tS-alowlbLYbrh-wtXCX5-eiQ`;
     // }
     return config;
 }, (error) => {
