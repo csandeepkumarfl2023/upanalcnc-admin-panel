08e717cd297aad5a7bd2a7ce3a0c973ce175e1e3
csandeepkumardev01 <sandeepk.dev01@gmail.com>
2021-06-28 11:03:05 -0700

    validation added for employee create

--- src/layout/EmployeeManagement/createEmployee.js
+++ src/layout/EmployeeManagement/createEmployee.js
@@ -5,7 +5,6 @@ import {
     CRow,
     CButton,
     CFormGroup,
-    CInput,
     CCardFooter,
     CAlert,
     CCardSubtitle,
@@ -18,6 +17,7 @@ import EmployeeService from '../../services/employeeService';
 import { useHistory } from "react-router-dom";
 import CIcon from '@coreui/icons-react'
 import * as Yup from "yup"
+import './styles.css'
 
 const employeeservice = new EmployeeService()
 
@@ -50,7 +50,6 @@ export default function CreateEmployee(props) {
     const submitHandler = async (value) => {
         try {
             console.log(value);
-
            let res = await employeeservice.createEmployee(value)
             history.push({
                 pathname: './employeemanagement',
@@ -76,6 +75,44 @@ var date = curr.toISOString().substr(0,10);
        {alertText}
       </CAlert>
             <Formik
+              validate={values => {
+                let errors = {};
+                if (!values.employee_id) {
+                  errors.employee_id = "Employee id is required";
+                }
+                if (!values.employee_name) {
+                    errors.employee_name = "Employee name is required";
+                  }
+                  if (!values.address) {
+                    errors.address = "Address is required";
+                  }
+                  if (!values.designation) {
+                    errors.designation = "Designation is required";
+                  }
+                  if (!values.email_id) {
+                    errors.email_id = "Designation is required";
+                  }
+                  if (!values.phone_number) {
+                    errors.phone_number = "Phone Number is required";
+                  }
+                  if (!values.security_pin) {
+                    errors.security_pin = "Security Pin is required";
+                  }
+                  if (!values.department) {
+                    errors.department = "Department is required";
+                  }
+                  if (!values.date_of_joining) {
+                    errors.date_of_joining = "Date of joining is required";
+                  }
+                  if (!values.date_of_leaving) {
+                    errors.date_of_leaving = "Date of leaving is required";
+                  }
+                  if (!values.employee_type) {
+                    errors.employee_type = "Employee Type is required";
+                  }
+                return errors;
+              }}
+              
                 initialValues={data}
                 validationSchema={validationSchema}
                 onSubmit={async (values) => {
@@ -90,25 +127,25 @@ var date = curr.toISOString().substr(0,10);
                         <CCardBody>
                             <div className="pt-3 pl-3">        
                                 <CRow className="pt-3 pb-2">
-                                
-                                    <CCol xs="12" sm="12" lg="6">
-                                    <CIcon name="cib-adobe-indesign" className='m-2'/> <b>Employee Id:</b>
-                                            <CFormGroup>
-                                                    <CInput style={{ width: '85%' }} type="text" id="employee_id" name="employee_id" placeholder="employee_id" onChange={handleChange} />
-                                                </CFormGroup>
+                                    <CCol xs="12" sm="12" lg="6">  
+                                    <CIcon name="cib-adobe-indesign" className='m-2'/>   <b>Employee Id:</b>
+                                                    <input style={{ width: '85%' }} type="text" id="employee_id" name="employee_id" placeholder="employee_id" 
+                                                    onChange={handleChange}  className={errors.employee_id && touched.employee_id && "error"}/>
+                                               
                                                 {errors.employee_id && touched.employee_id && 
-                                      <p style={{fontSize: 13, color: 'red'}} >{errors.employee_id}</p>}
+                                       <div className="input-feedback">{errors.employee_id}</div>}
                                     </CCol>
                                    
 
                                     <CCol xs="12" sm="12" lg="6">
                                     <CIcon name="cil-user" className='m-2'/> <b>Employee Name:</b>
                                                 <CFormGroup>
-                                                    <CInput type="text"
-                                                        style={{ width: '85%' }} id="employee_name" name="employee_name" placeholder="employee_name" onChange={handleChange} />
+                                                    <input type="text"
+                                                        style={{ width: '85%' }} id="employee_name" name="employee_name" placeholder="employee_name" 
+                                                        onChange={handleChange} className={errors.employee_name && touched.employee_name && "error"}/>
                                                 </CFormGroup>
                                                 {errors.employee_name && touched.employee_name && 
-                                      <p style={{fontSize: 13, color: 'red'}}>{errors.employee_name}</p>}
+                                      <div className="input-feedback">{errors.employee_name}</div>}
                                     </CCol>
                                 </CRow>
 
@@ -117,19 +154,21 @@ var date = curr.toISOString().substr(0,10);
                                     <CCol xs="12" sm="12" lg="6">
                                     <CIcon name="cil-location-pin" className='m-2'/><b>Address:</b>
                                             <CFormGroup>
-                                                    <CTextarea style={{ width: '85%' }} type="text" id="address" name="address" placeholder="Address" onChange={handleChange} />
+                                                    <CTextarea style={{ width: '85%' }} type="text" id="address" name="address" placeholder="Address" 
+                                                    onChange={handleChange} className={errors.address && touched.address && "error"}/>
                                                 </CFormGroup>
                                                 {errors.address && touched.address && 
-                                      <p style={{fontSize: 13, color: 'red'}}>{errors.address}</p>}
+                                      <div className="input-feedback">{errors.address}</div>}
                                     </CCol>
 
                                     <CCol xs="12" sm="12" lg="6">
                                     <CIcon name="cil-pen-alt" className='m-2'/><b>Designation:</b>
                                                 <CFormGroup>
-                                                    <CInput style={{ width: '85%' }} type="text" id="designation" name="designation" placeholder="Designation" onChange={handleChange} />
+                                                    <input style={{ width: '85%' }} type="text" id="designation" name="designation" placeholder="Designation" 
+                                                    onChange={handleChange} className={errors.designation && touched.designation && "error"}/>
                                                 </CFormGroup>
                                                 {errors.designation && touched.designation && 
-                                      <p style={{fontSize: 13, color: 'red'}}>{errors.designation}</p>}
+                                      <div className="input-feedback">{errors.designation}</div>}
                                     </CCol>
                                 </CRow>
 
@@ -137,19 +176,21 @@ var date = curr.toISOString().substr(0,10);
                                     <CCol xs="12" sm="12" lg="6">
                                     <CIcon name="cil-envelope-closed" className='m-2'/> <b>Email:</b>
                                             <CFormGroup>
-                                                <CInput style={{ width: '85%' }} type="text" id="email_id" name="email_id" placeholder="Email" onChange={handleChange} />
+                                                <input style={{ width: '85%' }} type="text" id="email_id" name="email_id" placeholder="Email"
+                                                 onChange={handleChange} className={errors.email_id && touched.email_id && "error"}/>
                                             </CFormGroup>
                                             {errors.email_id && touched.email_id && 
-                                      <p style={{fontSize: 13, color: 'red'}}>{errors.email_id}</p>}
+                                      <div className="input-feedback">{errors.email_id}</div>}
                                     </CCol>
                                     <CCol xs="12" sm="12" lg="6">
                                <CIcon name="cil-mobile" className='m-2'/> <b>Phone Number:</b>
                                         <CFormGroup>
-                                            <CInput type="text"
-                                                style={{ width: '85%' }} id="phone_number" name="phone_number" placeholder="phone_number" onChange={handleChange} />
+                                            <input type="text"
+                                                style={{ width: '85%' }} id="phone_number" name="phone_number" placeholder="phone_number" 
+                                                onChange={handleChange} className={errors.phone_number && touched.phone_number && "error"}/>
                                         </CFormGroup>
                                         {errors.phone_number && touched.phone_number && 
-                                      <p style={{fontSize: 13, color: 'red'}}>{errors.phone_number}</p>}
+                                      <div className="input-feedback">{errors.phone_number}</div>}
                             </CCol>
                                 </CRow>
                                 <CRow className="pt-3 pb-2">
@@ -158,20 +199,22 @@ var date = curr.toISOString().substr(0,10);
                             <CCol xs="12" sm="12" lg="6">
                             <CIcon name="cil-pin" className='m-2'/><b>Security Pin:</b>
                                         <CFormGroup>
-                                            <CInput type="text"
-                                                style={{ width: '85%' }} id="security_pin" name="security_pin" placeholder="security_pin" onChange={handleChange} />
+                                            <input type="text"
+                                                style={{ width: '85%' }} id="security_pin" name="security_pin" placeholder="security_pin" 
+                                                onChange={handleChange} className={errors.security_pin && touched.security_pin && "error"}/>
                                         </CFormGroup>
                                         {errors.security_pin && touched.security_pin && 
-                                      <p style={{fontSize: 13, color: 'red'}}>{errors.security_pin}</p>}
+                                      <div className="input-feedback">{errors.security_pin}</div>}
                             </CCol>
                             <CCol xs="12" sm="12" lg="6">
                         <CIcon name="cil-briefcase" className='m-2'/> <b>Department:</b>
                                         <CFormGroup>
-                                            <CInput type="text"
-                                                style={{ width: '85%' }} id="department" name="department" placeholder="department" onChange={handleChange} />
+                                            <input type="text"
+                                                style={{ width: '85%' }} id="department" name="department" placeholder="department" 
+                                                onChange={handleChange} className={errors.department && touched.department && "error"}/>
                                         </CFormGroup>
                                         {errors.department && touched.department && 
-                                      <p style={{fontSize: 13, color: 'red'}}>{errors.department}</p>}
+                                      <div className="input-feedback">{errors.department}</div>}
                             </CCol>
                         </CRow>
 
@@ -179,22 +222,24 @@ var date = curr.toISOString().substr(0,10);
                         <CCol xs="12" sm="12" lg="6">
                         <CIcon name="cil-calendar" className='m-2'/> <b>Date Of Joining:</b>
                            <CFormGroup >
-                                    <CInput style={{ width: '85%' }} type="date" id="date_of_joining"
+                                    <input style={{ width: '85%' }} type="date" id="date_of_joining"
                                        size="sm"
-                                       name="date_of_joining" placeholder="date_of_joining" onChange={handleChange} defaultValue={date}/>
+                                       name="date_of_joining" placeholder="date_of_joining" 
+                                       onChange={handleChange} defaultValue={date} className={errors.date_of_joining && touched.date_of_joining && "error"}/>
                                  </CFormGroup>
                                  {errors.date_of_joining && touched.date_of_joining && 
-                                      <p style={{fontSize: 13, color: 'red'}}>{errors.date_of_joining}</p>}
+                                      <div className="input-feedback">{errors.date_of_joining}</div>}
                             </CCol>
                             <CCol xs="12" sm="12" lg="6">
                             <CIcon name="cil-calendar" className='m-2'/><b>Date Of Leaving:</b>
                                         <CFormGroup>
-                                        <CInput style={{ width: '85%' }} type="date" id="date_of_leaving"
+                                        <input style={{ width: '85%' }} type="date" id="date_of_leaving"
                                        size="sm"
-                                       name="date_of_leaving" placeholder="date_of_leaving" onChange={handleChange} />
+                                       name="date_of_leaving" placeholder="date_of_leaving" 
+                                       onChange={handleChange} className={errors.date_of_leaving && touched.date_of_leaving && "error"}/>
                                         </CFormGroup>
                                         {errors.date_of_leaving && touched.date_of_leaving && 
-                                      <p style={{fontSize: 13, color: 'red'}}>{errors.date_of_leaving}</p>}
+                                      <div className="input-feedback">{errors.date_of_leaving}</div>}
                             </CCol>
                         </CRow>
                         <CRow className="pt-3 pb-2">
@@ -202,11 +247,12 @@ var date = curr.toISOString().substr(0,10);
                         <CCol xs="12" sm="12" lg="6">
                         <CIcon name="cil-voice-over-record" className='m-2'/><b>Employee Type:</b>
                                     <CFormGroup>
-                                        <CInput type="text"
-                                            style={{ width: '85%' }} id="employee_type" name="employee_type" placeholder="employee_type" onChange={handleChange} />
+                                        <input type="text"
+                                            style={{ width: '85%' }} id="employee_type" name="employee_type" placeholder="employee_type" 
+                                            onChange={handleChange} className={errors.employee_type && touched.employee_type && "error"}/>
                                     </CFormGroup>
                                     {errors.employee_type && touched.employee_type && 
-                                      <p style={{fontSize: 13, color: 'red'}}>{errors.employee_type}</p>}
+                                      <div className="input-feedback">{errors.employee_type}</div>}
                         </CCol>
                         <CCol xs="12" sm="12" lg="6">
                                     <CIcon name="cil-calendar-check" className='m-2'/><b>Active:</b>
